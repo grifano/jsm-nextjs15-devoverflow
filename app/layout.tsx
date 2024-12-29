cfe25bf19cf895d3a9a13b076fd19b2d0eef8ddb
Serhii Orlenko <146512960+grifano@users.noreply.github.com>
2024-12-29 13:14:29 +0100

    prepare project

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const interSans = Inter({
+  variable: "--font-Inter-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DevOverflow",
+  description: "A better version of Stack Overflow",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${interSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+const Home = () => (
+  <h1 className="text-3xl font-black">Welcome to the NextJS</h1>
+);
+
+export default Home;
