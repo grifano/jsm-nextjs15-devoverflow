5c6b29934dc8117e76aa61e44b224e739ceaf1a9
Serhii Orlenko <146512960+grifano@users.noreply.github.com>
2025-02-23 20:50:03 +0000

    add ShadCN component library, add dropdown-menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,39 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 const Home = () => (
   <>
-    <h1 className="flex text-3xl font-black text-light-400">
+    <h1 className="flex text-3xl font-black text-light-400 dark:text-2xl">
       Welcome to the NextJS
     </h1>
+
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        Open
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>
+          My Account
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          Profile
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          Billing
+        </DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>
+          Subscription
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   </>
 );
 
